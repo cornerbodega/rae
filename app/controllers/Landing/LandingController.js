823cc8910ec7aa40d05def409004fb3006924765
Cornerbodega <merhone@gmail.com>
2016-01-17 14:46:01 -0800

    where do I go?

--- app/controllers/Landing/LandingController.js
+++ app/controllers/Landing/LandingController.js
@@ -8,26 +8,25 @@
   function LandingController($location, ConnectService, Fire) {
     var vm = this;
     var formData = {}
-    var dummy = {username:'luchinisupercritical@gmail.com', password:'44Million!', ubi:'603347225'}
+    var dummy = {username:'thepottingbench@outlook.com', password:'2ndCaptainBly', ubi:'602093924'}
+    // var dummy = {username:'luchinisupercritical@gmail.com', password:'44Million!', ubi:'603347225'}
     var accounts = Fire.accounts
     vm.signIn = function () {
-        //are you registered with potnet? ie. do i have a ubi?
+        //are you registered with potnet? ie. do i have your username associated with a ubi?
+            // if not, return with error not found
         //can you lcblog in with data you've provided?
+            // if not, show lcb error message (nothing to do with me)
         // if (!Fire.usernameExists(dummy.username)) return
-        var ubi = ''
+        // var ubi = ''
         console.log(accounts);
-        // for(var key in accounts) {
-        //     if(accounts.hasOwnProperty(key)) {
-        //         if(accounts[key] === dummy.username) {
-        //             console.log('dummy un found');
-        //             ubi = accounts[ubi]
-        //         }
-        //     }
-        // }
-
-        if (ubi.length === 0) return vm.error = "Accountname not found."
-        dummy.ubi = ubi
-        ConnectService.lcbLogin(dummy, signUpLoginFail, function(loginRes){
+        var loginDummy = { username: 'thepottingkbench@outlook.com', password:'2ndCaptainBly'}
+        console.log(Fire.usernameExists(dummy.username));
+        // if (!Fire.usernameExists(dummy.username)) return vm.error = "Incorrect Username/Password"
+        // dummy.ubi =
+        loginDummy.ubi = Fire.getUbiFromUsername(dummy.username)
+        if (!loginDummy.ubi) return vm.error = "Incorrect Username/Password"
+        console.log(loginDummy.ubi);
+        ConnectService.lcbLogin(loginDummy, signUpLoginFail, function(loginRes){
             if(loginRes.success !=1) signUpLoginFail()
             console.log('Logged In successfully!');
             $location.path('/market')
@@ -40,11 +39,13 @@
             console.log('Logged In successfully!');
 
             //todolater console.log('check if ubi already in potnet db');
-            if(!!Fire.ubiExists(dummy)) {
-                return vm.error = 'UBI already registered with Potnet'
+            console.log(Fire.usernameExists(dummy));
+            if(Fire.usernameExists(dummy)) {
+                return vm.error = dummy.username +' is already registered with Potnet.net'
             }
             Fire.addAccount(dummy, function(key){
                 console.log('success! ! account added');
+                console.log('GO TO MARKET!');
                 $location.path('/market')
             })
 
--- app/services/Fire/Fire.js
+++ app/services/Fire/Fire.js
@@ -9,40 +9,47 @@ function Fire($firebaseObject) {
     // var FIREBASE_LOCATION =
     var rootRef = new Firebase("https://potnet.firebaseio.com")
     var accounts =  $firebaseObject(rootRef.child('accounts'))
+
     // var accounts = new Firebase("https://potnet.firebaseio.com/accounts")
     // var accounts = $firebaseObject(accountsRef);
+    function getUbiFromUsername(username) {
+        console.log(username);
+        return usernameExists({username:username})
+    }
     function addAccount(account, cb) {
-
-        console.log(account);
         if (!accounts[account.ubi]) accounts[account.ubi] = { users: []}
         accounts[account.ubi].users.push(account.username)
         console.log(accounts);
         accounts.$save()
-        // .then(function(ref) {
-        //     // ref.key() === obj.$id; // true
-        //     cb(ref.key)
-        // }, function(error) {
-        //     console.log("Error:", error);
-        // });
-        // accounts.$save().then(function(ref) {
-        //     ref.key() === obj.$id; // true
-        // }, function(error) {
-        //     console.log("Error:", error);
-        // });
+            .then(function(ref) {
+                // ref.key() === obj.$id; // true
+                cb(ref.key)
+            }, function(error) {
+                console.log("Error:", error);
+        });
     }
 
-    function ubiExists(account) {
+    function usernameExists(account) {
         // var accountsRef = new Firebase("https://potnet.firebaseio.com/accounts")
         console.log(accounts);
+        console.log(account);
+        var r = false
         _.each(accounts, function(a){
             if (!!a && !!a.users) {
                 _.each(a.users, function (u){
-                    if (u === account.username) return true
+                    console.log(u);
+                    console.log(account.username);
+                    if (u === account.username) {
+                        console.log('exists!!');
+                        r = u
+                    }
                 })
             }
-            return false
+            // console.log();
 
         })
+        return r
+
         // var accountsRef = accounts
     // accountsRef.orderByChild("email").on("child_added", function(snapshot){
     //     console.log(snapshot.val().email);
@@ -86,8 +93,9 @@ function Fire($firebaseObject) {
     }
 
     return {
-        ubiExists: ubiExists,
+        usernameExists: usernameExists,
         addAccount: addAccount,
         accounts: accounts,
+        getUbiFromUsername: getUbiFromUsername,
     }
 }
